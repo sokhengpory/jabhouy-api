835748aefb99c0c0ceb34bca29c68b67cc2d5795
Pory Sokheng <porysokheng@gmail.com>
2025-03-04 23:06:21 +0700

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import { serve } from '@hono/node-server';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { apiReference } from '@scalar/hono-api-reference';
+import { pinoLogger } from 'hono-pino';
+import pino from 'pino';
+import pretty from 'pino-pretty';
 import { auth } from './lib/auth';
+import type { AppBindings } from './lib/type';
 import { authMiddleware } from './middleware/auth';
 import { itemRoute } from './route/item';
 
-const openApiApp = new OpenAPIHono();
+const openApiApp = new OpenAPIHono<AppBindings>();
 
-openApiApp.use('*', authMiddleware);
+openApiApp.use(authMiddleware);
+openApiApp.use(
+	pinoLogger({
+		pino: pino(
+			{
+				level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+			},
+			process.env.NODE_ENV === 'production' ? undefined : pretty(),
+		),
+	}),
+);
 
 openApiApp.onError((err, c) => {
 	console.log(err);
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,8 +1,10 @@
+import type { PinoLogger } from 'hono-pino';
 import type { auth } from './auth';
 
 export interface AppBindings {
 	Variables: {
 		user: typeof auth.$Infer.Session.user | null;
 		session: typeof auth.$Infer.Session.session | null;
+		logger: PinoLogger;
 	};
 }
