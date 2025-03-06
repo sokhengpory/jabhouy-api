16f0a52d621dc53f8050d047e41c8d2f044ab1cb
Pory Sokheng <porysokheng@gmail.com>
2025-03-06 20:49:14 +0700

    add pagination on items

--- src/route/items/list-item.ts
+++ src/route/items/list-item.ts
@@ -2,7 +2,7 @@ import { db } from '@/db';
 import { itemSchema, items } from '@/db/schema';
 import type { AppRouteHandler } from '@/lib/type';
 import { createRoute, z } from '@hono/zod-openapi';
-import { and, eq, like } from 'drizzle-orm';
+import { and, count as countFn, eq, like } from 'drizzle-orm';
 import * as HttpStatusCodes from 'stoker/http-status-codes';
 import { jsonContent } from 'stoker/openapi/helpers';
 
@@ -13,10 +13,23 @@ export const itemsRoute = createRoute({
 	request: {
 		query: z.object({
 			search: z.string().optional(),
+			page: z.string().optional().default('1'),
+			pageSize: z.string().optional().default('25'),
 		}),
 	},
 	responses: {
-		[HttpStatusCodes.OK]: jsonContent(z.array(itemSchema), 'List items'),
+		[HttpStatusCodes.OK]: jsonContent(
+			z.object({
+				items: z.array(itemSchema),
+				pagination: z.object({
+					total: z.number(),
+					page: z.number(),
+					pageSize: z.number(),
+					totalPages: z.number(),
+				}),
+			}),
+			'List items with pagination',
+		),
 	},
 });
 
@@ -24,17 +37,40 @@ export const listItemHandler: AppRouteHandler<typeof itemsRoute> = async (
 	c,
 ) => {
 	const userId = c.var.user.id;
-	const { search } = c.req.valid('query');
+	const { search, page, pageSize } = c.req.valid('query');
 
-	const result = await db
+	const pageNumber = Number.parseInt(page);
+	const limit = Number.parseInt(pageSize);
+	const offset = (pageNumber - 1) * limit;
+
+	const filters = [eq(items.userId, userId)];
+
+	if (search) {
+		filters.push(like(items.name, `%${search}%`));
+	}
+
+	const [{ count }] = await db
+		.select({ count: countFn() })
+		.from(items)
+		.where(and(...filters));
+
+	const results = await db
 		.select()
 		.from(items)
-		.where(
-			and(
-				eq(items.userId, userId),
-				search ? like(items.name, `%${search}%`) : undefined,
-			),
-		);
+		.where(and(...filters))
+		.limit(limit)
+		.offset(offset);
 
-	return c.json(result, HttpStatusCodes.OK);
+	return c.json(
+		{
+			items: results,
+			pagination: {
+				total: count,
+				page: pageNumber,
+				pageSize: limit,
+				totalPages: Math.ceil(count / limit),
+			},
+		},
+		HttpStatusCodes.OK,
+	);
 };
