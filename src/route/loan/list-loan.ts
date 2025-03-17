a23332f5bcda808a20a672cb94ff3a6aed873fa5
Pory Sokheng <porysokheng@gmail.com>
2025-03-17 22:15:41 +0700

    search loan by name

--- src/route/loan/list-loan.ts
+++ src/route/loan/list-loan.ts
@@ -1,8 +1,8 @@
 import { createRoute } from '@hono/zod-openapi';
-import { eq, getTableColumns } from 'drizzle-orm';
+import { z } from '@hono/zod-openapi';
+import { and, eq, getTableColumns, like, or } from 'drizzle-orm';
 import * as HttpStatusCodes from 'stoker/http-status-codes';
 import { jsonContent } from 'stoker/openapi/helpers';
-import { z } from 'zod';
 import { db } from '~/db';
 import { loan, selectLoanSchema } from '~/db/schema';
 import type { AppRouteHandler } from '~/lib/type';
@@ -11,6 +11,11 @@ export const listLoanRoute = createRoute({
 	method: 'get',
 	path: '/',
 	tags: ['Loan'],
+	request: {
+		query: z.object({
+			name: z.string().optional(),
+		}),
+	},
 	responses: {
 		[HttpStatusCodes.OK]: jsonContent(
 			z.array(selectLoanSchema),
@@ -22,16 +27,23 @@ export const listLoanRoute = createRoute({
 export const listLoanHandler: AppRouteHandler<typeof listLoanRoute> = async (
 	c,
 ) => {
+	const { name } = c.req.valid('query');
 	const user = c.var.user;
 
 	const { userId, ...rest } = getTableColumns(loan);
+
+	const filters = [eq(loan.userId, user.id)];
+
+	if (name) {
+		filters.push(like(loan.name, `%${name}%`));
+	}
 
 	const results = await db
 		.select({
 			...rest,
 		})
 		.from(loan)
-		.where(eq(loan.userId, user.id))
+		.where(and(...filters))
 		.orderBy(loan.createdAt);
 
 	return c.json(results);
