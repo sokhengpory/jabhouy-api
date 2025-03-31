6bf1fabdea9afaa58cf6a68b83b276d0c0a4182b
Pory Sokheng <porysokheng@gmail.com>
2025-03-31 15:51:48 +0700

    allow reference docs when in preview

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,6 +6,13 @@ export const authMiddleware: MiddlewareHandler = async (c, next) => {
 		return next();
 	}
 
+	if (
+		c.req.path.startsWith('/reference') ||
+		(c.req.path.startsWith('/docs') && process.env.NODE_ENV !== 'production')
+	) {
+		return next();
+	}
+
 	const session = await auth.api.getSession({ headers: c.req.raw.headers });
 	if (!session) {
 		return c.json({ code: 'Unauthorized' }, 401);
